c5244fe1724a2ce11c0402f632062d5e2f14d31f
Diurno <Diurno@C08PC10.informatica.comercio>
2024-03-27 11:41:50 +0100

    Codigo inicial

---
+++ src/main/java/clase/pruebaed_2a_ivancompanon/PruebaED_2a_IvanCompanon.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package clase.pruebaed_2a_ivancompanon;
+
+/**
+ *
+ * @author Diurno
+ */
+public class PruebaED_2a_IvanCompanon {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
